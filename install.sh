32c37572db4bab338a1fe680029ca764ebd33080
Ryan Boehning <1250684+ryboe@users.noreply.github.com>
2023-01-24 01:16:19 +0000

    Add support for Debian-based codespaces

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-install_tools() {
+install_tools_alpine() {
     sudo apk add --update bat exa fd file fzf neovim ripgrep shfmt
 
     sudo wget --quiet --timeout=30 --output-document=/usr/local/bin/gitprompt https://github.com/ryboe/gitprompt/releases/latest/download/gitprompt-x86_64-unknown-linux-musl
@@ -11,6 +11,27 @@ install_tools() {
     # Install fzf shell integration functions to /usr/share/fzf.
     sudo wget --quiet --timeout=30 --output-document=/usr/share/fzf/completion.zsh https://raw.githubusercontent.com/junegunn/fzf/master/shell/completion.zsh
     sudo wget --quiet --timeout=30 --output-document=/usr/share/fzf/key-bindings.zsh https://raw.githubusercontent.com/junegunn/fzf/master/shell/key-bindings.zsh
+}
+
+install_tools_debian() {
+    sudo apt install -y --no-install-recommends bat exa fd-find fzf neovim ripgrep golang-mvdan-sh
+    sudo ln -s /usr/bin/batcat ~/usr/bin/bat
+    sudo wget --quiet --timeout=30 --output-document=/usr/local/bin/gitprompt https://github.com/ryboe/gitprompt/releases/latest/download/gitprompt-x86_64-unknown-linux-gnu
+    sudo chmod +x /usr/local/bin/gitprompt
+
+    sudo wget --quiet --timeout=30 --output-document=/usr/share/fzf/completion.zsh https://raw.githubusercontent.com/junegunn/fzf/master/shell/completion.zsh
+    sudo wget --quiet --timeout=30 --output-document=/usr/share/fzf/key-bindings.zsh https://raw.githubusercontent.com/junegunn/fzf/master/shell/key-bindings.zsh
+}
+
+install_tools() {
+    if [[ -f /etc/alpine-release ]]; then
+        install_tools_alpine
+    elif [[ $(command -v lsb_release) && $(lsb_release --id --short) == Debian ]]; then
+        install_tools_debian
+    else
+        echo 'ERROR: unknown linux distribution'
+        echo 'skipping installation of command line tools'
+    fi
 }
 
 create_symlinks() {
